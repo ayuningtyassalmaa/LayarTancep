9c42911883687ef380626da428406c64f84643c6
Salma Ayuningtyas <ayuningtyas.ayusalma@gmail.com>
2025-11-07 23:40:22 +0700

    changes (1): - add modules regards to MVVM patern - add Dashboard Page - add reusable view for header section - add documentation

---
+++ LayarTancep/Modules/View/Dashboard/PopularMoviesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PopularMoviesCollectionViewCell.swift
+//  LayarTancep
+//
+//  Created by Salma Ayuningtyas on 11/7/25.
+//
+
+import UIKit
+
+class PopularMoviesCollectionViewCell: UICollectionViewCell {
+    
+}
