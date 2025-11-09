ad35d724bca2cc33d8275fd2756f6c2bc2d7355d
Salma Ayuningtyas <ayuningtyas.ayusalma@gmail.com>
2025-11-09 23:06:31 +0700

    changes (3): - add setup endpoints and API Services - add setup view model - add binding vc to view model - add model data (popular movies list)

---
+++ LayarTancep/Modules/APIServices.swift
@@ -0,0 +1,8 @@
+//
+//  APIServices.swift
+//  LayarTancep
+//
+//  Created by Salma Ayuningtyas on 11/9/25.
+//
+
+import Foundation
---
+++ LayarTancep/Modules/Model/PopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  PopularMovies.swift
+//  LayarTancep
+//
+//  Created by Salma Ayuningtyas on 11/9/25.
+//
+
+import Foundation
